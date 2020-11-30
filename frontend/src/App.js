5729fd64dfa0029e30deee9b6e3f444173144eb2
Vikas <victorjonathon@gmail.com>
2020-11-30 09:39:52 +0530

    Initial page in React

---
+++ frontend/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap';
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to App</h1> 
+        </Container>
+        
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
